56cb5ab1127b41f477a554d5c6ee11cca1a09741
Enrique Lopez <morelearning2025@gmail.com>
2024-10-17 15:31:38 +0000

    SUPER TODOLIST EnriqueLOpez4G

--- src/js/component/MainCard.jsx
+++ src/js/component/MainCard.jsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const MainCard =()=> {
     const [inputValue, setInputValue] = useState('');
     const [todos, setTodos] = useState([]);
+    const [hoveredIndex, setHoveredIndex] = useState(null); // Estado para el hover
 
     //tuve que usar esto, porque me marcaba deprecated con el OnkeyPress
     const handleKeyDown = (event) => {
@@ -15,30 +16,83 @@ const MainCard =()=> {
       const handleDelete = (index) => {
         const newTodos = todos.filter((_, i) => i !== index);
         setTodos(newTodos);
+        alert('Elemento Aniquilado')
       };
-    
+
     return (
-        
-        <div className="card" style={{ width: '500px', backgroundColor: 'rgb(240, 240, 240)' }}>
+        <div className="card" style={{ 
+                                      width           : '500px', 
+                                      backgroundColor : 'rgb(240, 240, 240)', 
+                                      boxShadow       : '0 4px 8px rgba(0, 0, 0, 0.2)',
+                                      margin          : '10px 0' 
+                                    }}
+        >
+          <p className =" text-center" style = {{
+                                                  color      : 'rgba(86,7,12,0.5)',  
+                                                  fontSize   : '5rem', 
+                                                  fontWeight : '200' 
+                                                }}
+          >
+				    todos
+			    </p>
           <div className="card-body">
             <input
-              type="text"
-              className="form-control"
-              onChange={(e) => setInputValue(e.target.value)}
-              style={{ backgroundColor: 'white', color: 'black' }}
-              placeholder="Write new activiy here..."
-              onKeyDown={handleKeyDown} 
+              type        = "text"
+              className   = "form-control"
+              onChange    = {(e) => setInputValue(e.target.value)}
+              style       = {{ backgroundColor: 'white', color: 'black' }} //linea par estilos del input
+              placeholder = "Write new activiy here..."
+              onKeyDown   = {handleKeyDown} 
+              value       = {inputValue} //contenido del input sincronizado con el state
             />
             <ul>
                 {todos.map((todo, index) => (
                 <li key={index} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <span style = {{ fontFamily: 'Cinzel, serif', fontSize: '2rem'}}>{todo}</span>
-                    <button onClick={() => handleDelete(index)} style={{ border: 'none', background: 'none', color: 'red', cursor: 'pointer' }}>
-                    &#10006; {/* Icono de tache */}
-                    </button>
+                    <span 
+                      style = {{ 
+                                  fontFamily : 'system-ui', 
+                                  fontSize   : '1.5rem', 
+                                  fontWeight : '100',
+                                  color      : hoveredIndex===index ? '#F00':'#474B4E' //con esto cambio a rojo si hay hover
+
+                      }}
+                      onMouseEnter={() => setHoveredIndex(index)} // Detectar hover
+                      onMouseLeave={() => setHoveredIndex(null)}  // Limpiar hover
+                      
+                    >
+                      {todo}
+                    </span>
+                    <button
+                      onClick={() => handleDelete(index)} 
+                      onMouseEnter={() => setHoveredIndex(index)} // Detectar hover
+                      onMouseLeave={() => setHoveredIndex(null)}  // Limpiar hover
+                      style={{ 
+                                border       : 'none', 
+                                borderRadius : '50%',
+                                background   : 'white',
+                                cursor       : 'pointer',
+                                opacity      : hoveredIndex === index ? 1 : 0, // Mostrar solo si coincide el índice
+                                transition   : 'opacity 0.3s ease', //suave suave suavecito
+                              }}
+                    >
+                     {/* Icono de tache &#10006; nunca pude ponerlo en rojo caray! */}
+                     <span
+                      src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi27ThgGKW7oZdbq8yWtM2rW0r6Yhtm8ZWrFGxjY6fhJ9dR6hl5AR1Ds5fVTAMk768Yes&usqp=CAU" 
+                      alt = "Red Cross Icon" 
+                      style = {{ 
+                              width       : '24px', 
+                              height      : '24px',  
+                            }} 
+                    >
+                      ❌
+                    </span>
+                  </button>
                 </li>
                 ))}
             </ul>
+            <hr/>
+            <p style = {{opacity:'0.5'}}><strong>{todos.length}</strong>  Activities in the list</p>
+            <p className = "text-end" style = {{color: 'blue',opacity:'0.7'}}><strong></strong>🏆Enrique Lopez 4G</p>
           </div>
         </div>
     )
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,9 +5,6 @@ import MainCard from "./MainCard";
 const Home = () => {
 	return (
 		<div className="container vh-100 d-flex flex-column justify-content-center align-items-center">
-			<p style = {{color: 'rgba(86,7,12,0.5)',  fontSize: '6rem', fontWeight: '200' }}>
-				todos
-			</p>
 			<MainCard/>
 		</div>
 	);
